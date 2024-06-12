a2df7457833dc2e567db2eefd5852d9ed9c97424
16872 <1687289734@qq.com>
2024-06-12 23:22:47 +0800

    完成阿里云实习机试

---
+++ src/main/java/problems/alibaba/cloud_240512/EvenPair.java
@@ -0,0 +1,50 @@
+package main.java.problems.alibaba.cloud_240512;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @program: JobCoding
+ * @author: PangTeen
+ * @create: 2024/6/12 23:06
+ **/
+public class EvenPair {
+
+    private static long ans = 0;
+
+    /**
+     * 遍历找不相邻偶数累计答案。
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        List<Integer>[] edges = new List[n + 1];
+        for(int i = 0; i <= n; ++ i) edges[i] = new ArrayList<>();
+        for(int i = 1; i < n; ++ i){
+            int u = scanner.nextInt();
+            int v = scanner.nextInt();
+            edges[u].add(v);
+            edges[v].add(u);
+        }
+        dfs(edges, 1, 0, n);
+        System.out.println(ans >> 1);
+    }
+
+    private static void dfs(List<Integer>[] edges, int now, int fa, int n){
+        int closeEven = (now % 2 == 0 ? 1 : 0);
+        for(int nxt : edges[now]){
+            if(nxt % 2 == 0) closeEven ++;
+        }
+        if(now % 2 == 0){
+            ans += n - 1 - edges[now].size();
+        } else {
+            ans += n / 2 - closeEven;
+        }
+        for(int nxt : edges[now]){
+            if(nxt != fa){
+                dfs(edges, nxt, now, n);
+            }
+        }
+    }
+}
---
+++ src/main/java/problems/alibaba/cloud_240512/_01Sequence.java
@@ -0,0 +1,42 @@
+package main.java.problems.alibaba.cloud_240512;
+
+import java.util.Scanner;
+
+/**
+ * @program: JobCoding
+ * @author: PangTeen
+ * @create: 2024/6/12 22:55
+ **/
+public class _01Sequence {
+
+    /**
+     * 前缀和。
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int q = scanner.nextInt();
+        String s = scanner.next();
+        int[][] pre = new int[n + 1][2];
+        for(int i = 0; i < 2; ++ i){
+            for(int j = 0; j < n; ++ j){
+                if(i == 0){
+                    if((j & 1) == 0 ^ s.charAt(j) == '0') pre[j + 1][i] = 1;
+                } else {
+                    if((j & 1) == 0 ^ s.charAt(j) == '1') pre[j + 1][i] = 1;
+                }
+            }
+        }
+        for(int i = 0; i < n; ++ i){
+            for(int j = 0; j < 2; ++ j){
+                pre[i + 1][j] += pre[i][j];
+            }
+        }
+        while(q -- > 0){
+            int l = scanner.nextInt();
+            int r = scanner.nextInt();
+            int ans = Math.min((pre[r][0] - pre[l - 1][0]), (pre[r][1] - pre[l - 1][1]));
+            System.out.println(ans);
+        }
+    }
+}
